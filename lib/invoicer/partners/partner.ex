167a74acab0219630683e7705cfa9d4144053122
László Balassa <b.lacika23@gmail.com>
2022-12-29 16:43:55 +0100

    Change properties

--- lib/invoicer/partners/partner.ex
+++ lib/invoicer/partners/partner.ex
@@ -5,13 +5,13 @@ defmodule Invoicer.Partners.Partner do
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "partners" do
-    field :active, :boolean, default: false
+    field :active, :boolean, default: true
     field :address_ids, {:array, :string}
     field :bank_account_ids, {:array, :string}
     field :my_company, :boolean, default: false
     field :name, :string
     field :payment_method, :string
-    field :payment_therm, :integer, default: 0
+    field :payment_term, :integer, default: 0
     field :type, :string
     field :vatnumber, :string
     field :currency_id, :binary_id
@@ -22,7 +22,7 @@ defmodule Invoicer.Partners.Partner do
   @doc false
   def changeset(partner, attrs) do
     partner
-    |> cast(attrs, [:my_company, :name, :active, :type, :vatnumber, :bank_account_ids, :payment_therm, :payment_method, :address_ids])
+    |> cast(attrs, [:my_company, :name, :active, :type, :vatnumber, :bank_account_ids, :payment_term, :payment_method, :address_ids, :currency_id])
     |> validate_required([:my_company, :name])
     |> unique_constraint(:vatnumber)
     |> unique_constraint(:name)
